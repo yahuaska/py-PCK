9a8d7ee134f1fcbae3dd6e953aca26ba7eb311ce
Ringo <ringo.working@gmail.com>
2014-12-17 20:07:22 +0300

    Simple setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py-PCK',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/yahuaska/py-PCK',
+    license='GNU GPL v2.0',
+    author='yahuaska',
+    author_email='ringo.working@gmail.com',
+    description='Python class for manipulating PCK images from X-Com: Apocalypse. Based on code from OpenApoc and ported from C++.'
+)
